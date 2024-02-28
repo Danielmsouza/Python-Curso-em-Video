6927e8e7cc23a10259b7228c006886734d3ad43b
Daniel <daniel.msouza@hotmail.com>
2024-02-28 18:46:29 -0300

    exercicios

---
+++ exercicios/ex007.py
@@ -0,0 +1,8 @@
+'''Program that reads the 2 notes and calculates the average '''
+'''Programa que le as 2 notas e calcula a media'''
+
+note1 = int(input('Enter the first note: '))
+note2 = int(input('Enter the second note: '))
+average = (note1 + note2)/2
+
+print(f'The average of the grades {note1} and {note2} is {average}')
