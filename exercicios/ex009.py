6e68f5e9785f517ebb3002f97c708786e11c7f2b
Daniel <daniel.msouza@hotmail.com>
2024-08-16 10:03:11 -0300

    add

---
+++ exercicios/ex009.py
@@ -0,0 +1,15 @@
+'''Programm that reads a number and shows the multiplication table '''
+'''Programa que le um numero inteiro e mostra a tabuada'''
+
+number = int(input('Enter the number: '))
+print(f'0 x {number} = {number*0}')
+print(f'1 x {number} = {number*1}')
+print(f'2 x {number} = {number*2}')
+print(f'3 x {number} = {number*3}')
+print(f'4 x {number} = {number*4}')
+print(f'5 x {number} = {number*5}')
+print(f'6 x {number} = {number*6}')
+print(f'7 x {number} = {number*7}')
+print(f'8 x {number} = {number*8}')
+print(f'9 x {number} = {number*9}')
+print(f'10 x {number} = {number*10}')
